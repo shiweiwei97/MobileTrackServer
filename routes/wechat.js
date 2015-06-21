7808c99b99ff40fea2e5f6dd7176ff6282453dd8
shiweiwei97 <shiweiwei97@gmail.com>
2015-06-21 19:45:57 +0800

    wechat update

--- routes/wechat.js
+++ routes/wechat.js
@@ -5,11 +5,17 @@
 var express     = require('express'),
     log         = require('log4js').getLogger(),
     wechat      = require('wechat'),
+    appid       = '45243392f153e83de593417bb2266c80',
     token       = 'wechat_token',
     secrect     = '45243392f153e83de593417bb2266c80',
+    config      = {
+        token: token,
+        appid: appid,
+        encodingAESKey: secrect
+    },
     router;
 
-router = wechat(token, secrect, function (req, res, next) {
+router = wechat(token, function (req, res, next) {
     var message = req.weixin;
     log.debug(message);
 
